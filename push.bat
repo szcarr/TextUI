e1ec93ec73fbe66fb824b525607df0b3b2d827ee
szcarr <itwangzy@gmail.com>
2022-03-31 21:03:58 +0200

    New version

--- push.bat
+++ push.bat
@@ -1,5 +1,6 @@
 REM Use this batch file to push thid folder to github
 REM Place in folder you want to upload
+REM Replace origin link to your own repository
 git init
 git add .
 git commit -m "New version"
@@ -7,4 +8,4 @@ git remote rm origin
 git remote add origin https://github.com/szcarr/TextUI.git
 git pull --rebase origin master
 git push origin master
-pause
+REM pause
