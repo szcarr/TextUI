e714ced7aff24c90509862f421eed1e653d0bf75
szcarr <itwangzy@gmail.com>
2022-03-31 21:01:18 +0200

    New version

--- push.bat
+++ push.bat
@@ -5,6 +5,6 @@ git add .
 git commit -m "New version"
 git remote rm origin
 git remote add origin https://github.com/szcarr/TextUI.git
-git pull --rebase origin main
-git push origin main
+git pull --rebase origin master
+git push origin master
 pause
